52fb0e1ca8b3ba667d1ab174d874bcc07a8c4402
anamaksimova <19cherrytree91@gmail.com>
2022-03-19 14:12:25 +0300

    task2

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,8 +1,16 @@
-alert('hi');
-const productName = 'milk';
+'use strict'
+{const productName = 'milk';
 const quantity = 2;
 const category = 'milkProducts';
 const price = 22;
-
 console.log(productName);
-console.log(quantity * price);
+console.log(`общая сумма товара: ${quantity * price}`);
+}
+{
+const productName = 'bread';
+const quantity = 5;
+const category = 'bakery';
+const price = 10;
+console.log(productName);
+console.log(`общая сумма товара: ${quantity * price}`);
+}
