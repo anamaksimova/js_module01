a5bdf4224640a3c0047a21a02d0034e6155bafa9
anamaksimova <19cherrytree91@gmail.com>
2022-04-11 18:03:19 +0300

    lesson11

--- lesson10/cart1.js
+++ lesson10/cart1.js
@@ -1,55 +1,49 @@
-'use strict'
+'use strict';
 const cart = {
-    items: [],
-    get totalPrice(){
-        return this.calculateItemPrice();
-    },
-    set totalPrice(total){
-        this.totalPrice = total;
-    },
-    count: 0,
+  items: [],
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+  set totalPrice(total) {
+    this.totalPrice = total;
+  },
+  count: 0,
 
-    add(itemName, price, quantity = 1) {
-        let newObj = {
-            itemName: itemName,
-            price: price,
-            quantity:quantity
-        };
-        console.log(newObj);
-        cart.items.push(newObj);
-        cart.increaseCount(quantity);
+  add(itemName, price, quantity = 1) {
+    const newObj = {
+      itemName,
+      price,
+      quantity,
+    };
+    console.log(newObj);
+    cart.items.push(newObj);
+    cart.increaseCount(quantity);
+  },
 
-    },
+  increaseCount(plusQuantity) {
+    this.count += plusQuantity;
+  },
 
-    increaseCount(plusQuantity) {
-        for (const item of cart.items){
-            const {itemName, price, quantity} = item;
-            }
-        this.count += plusQuantity; 
-        
-    },
+  calculateItemPrice() {
+    let totalPrice = 0;
+    for (const item of cart.items) {
+      const {itemName, price, quantity} = item;
+      totalPrice += price * quantity;
+    }
+    return totalPrice;
+  },
 
-    calculateItemPrice(){
-        let totalPrice = 0;
-        for (const item of cart.items){
-            const {itemName, price, quantity} = item;
-            totalPrice += price * quantity;
-            } 
-        return totalPrice;
-        
-        },
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
 
-    clear(){
-        this.items = [];
-        this.count = 0;
-    },
-
-    print(){
-        const itemsStr = JSON.stringify(cart.items);
-        console.log(itemsStr);
-        console.log('total price: ' + this.totalPrice);
-    },
-}
+  print() {
+    const itemsStr = JSON.stringify(cart.items);
+    console.log(itemsStr);
+    console.log('total price: ' + this.totalPrice);
+  },
+};
 console.log('Привет');
 
 cart.add('milk', 20, 1);
@@ -59,6 +53,5 @@ cart.add('tomato', 50);
 cart.print();
 
 console.log('cart.totalPrice(): ', cart.totalPrice);
-
 
 
--- lesson10/task2.js
+++ lesson10/task2.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 const unicIPNumber = arr => {
-    const listOfIp = [...new Set(arr)];
-    return listOfIp.length;
-}
-console.log("Число уникальных IP-адресов: " + unicIPNumber(listIPv4));
+  const listOfIp = [...new Set(arr)];
+  return listOfIp.length;
+};
+console.log('Число уникальных IP-адресов: ' + unicIPNumber(listIPv4));
